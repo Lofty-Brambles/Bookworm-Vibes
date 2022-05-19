f42c41fd8cb89fcc01b22ec4f6d24e19ccc56635
Lofty | Brambles <prantikdas2004@gmail.com>
2022-05-19 15:39:08 +0530

    💢 | Added refresh and drop menu [unfinished]

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ console.log( "test" );
 const date = document.querySelector( ".date" );
 date.textContent = ( new Date() ).getFullYear();
 
+// Adds book reset with a refresh
+
+const settings = document.querySelector( "#settings" );
+settings.addEventListener( "click", () => { location.reload(); } );
+
+// Colour settings
+
+const colorbtns = document.querySelectorAll( "#image > ul li" );
+colorbtns.forEach( btn => {
+	btn.addEventListener( "click", e => {
+
+		e.currentTarget.className += " active";
+		console.log("btn activated");
+
+		colorbtns.forEach( btnd => {
+			btnd.className = btnd.className
+				.replace( " active", "" );
+			console.log("btn deactivated");
+		} );
+
+	} );
+} );
+
 // Book arrays and interchange
 
 function Book( name, auth, pages, read ) {
@@ -17,4 +40,3 @@ function Book( name, auth, pages, read ) {
 }
 
 const book = new Book("jej");
-console.log( book.ask() );
